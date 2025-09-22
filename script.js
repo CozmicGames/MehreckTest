fce049685e81db89aea9e29755029f95c2bdf24a
CozmicGames <max.schott@hotmail.de>
2025-09-22 21:48:25 +0200

    Add touch input

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ scaleSlider.addEventListener("input", e => {
 setup(currentCategoryCount);
 
 
-/*
+/**
 *   Setup graph
 */
 function setup(categoryCount) {
@@ -50,68 +50,27 @@ function setup(categoryCount) {
     });
 
     canvas.addEventListener("mousedown", e => {
-        const rect = canvas.getBoundingClientRect();
-        const mouseX = e.clientX - rect.left;
-        const mouseY = e.clientY - rect.top;
-        const graphRadius = getGraphRadius(canvas);
-
-        for (let i = 0; i < categoryCount; i++) {
-            const handle = dataHandles[i];
-            const dataPoint = dataPoints[handle.index];
-            const angle = getCategoryAngle(handle.index, categoryCount);
-
-            const handleX = canvas.width / 2 + graphRadius * dataPoint * Math.cos(angle);
-            const handleY = canvas.height / 2 + graphRadius * dataPoint * Math.sin(angle);
-            
-            const dx = mouseX - handleX;
-            const dy = mouseY - handleY;
-
-            if (dx * dx + dy * dy <= 8 * 8) {
-                draggedHandle = handle;
-                offsetX = dx;
-                offsetY = dy;
-                break;
-            }
-        }
+        onInputDown(e.clientX, e.clientY, 0);
     });
 
     canvas.addEventListener("mousemove", e => {
-        if (!draggedHandle) return;
-
-        const rect = canvas.getBoundingClientRect();
-        const mouseX = e.clientX - rect.left - offsetX;
-        const mouseY = e.clientY - rect.top - offsetY;
-        const graphRadius = getGraphRadius(canvas);
-
-        const angle = getCategoryAngle(draggedHandle.index, categoryCount);
-        
-        const lineStart = {
-            x: canvas.width / 2,
-            y: canvas.height / 2
-        }
-
-        const lineEnd = {
-            x: lineStart.x + graphRadius * Math.cos(angle),
-            y: lineStart.y + graphRadius * Math.sin(angle)
-        }
-
-        const APx = mouseX - lineStart.x;
-        const APy = mouseY - lineStart.y;
-        const ABx = lineEnd.x - lineStart.x;
-        const ABy = lineEnd.y - lineStart.y;
-        const ab2 = ABx * ABx + ABy * ABy;
-        let t = (APx * ABx + APy * ABy) / ab2;
-        t = Math.max(0, Math.min(1, t));
-
-        dataPoints[draggedHandle.index] = t;
-
-        draggedHandle.isValid = true;
-
-        draw();
+        onInputMove(e.clientX, e.clientY);
     });
 
     canvas.addEventListener("mouseup", () => {
-        draggedHandle = null;
+        onInputUp();
+    });
+
+    canvas.addEventListener("touchstart", e => {
+        onInputDown(e.clientX, e.clientY, 3);
+    });
+
+    canvas.addEventListener("touchmove", e => {
+        onInputMove(e.clientX, e.clientY);
+    });
+
+    canvas.addEventListener("touchend", () => {
+        onInputUp();
     });
 
     canvas.width = window.innerWidth * 0.8;
@@ -119,7 +78,75 @@ function setup(categoryCount) {
     draw();
 }
 
-/*
+/**
+ *  Input
+ */
+function onInputDown(x, y, size) {
+    const rect = canvas.getBoundingClientRect();
+    const inputX = x - rect.left;
+    const inputY = y - rect.top;
+    const graphRadius = getGraphRadius(canvas);
+
+    for (let i = 0; i < categoryCount; i++) {
+        const handle = dataHandles[i];
+        const dataPoint = dataPoints[handle.index];
+        const angle = getCategoryAngle(handle.index, categoryCount);
+
+        const handleX = canvas.width / 2 + graphRadius * dataPoint * Math.cos(angle);
+        const handleY = canvas.height / 2 + graphRadius * dataPoint * Math.sin(angle);
+        
+        const dx = inputX - handleX;
+        const dy = inputY - handleY;
+
+        if (dx * dx + dy * dy <= 8 * 8 + size * size) {
+            draggedHandle = handle;
+            offsetX = dx;
+            offsetY = dy;
+            break;
+        }
+    }
+}
+
+function onInputMove(x, y) {
+    if (!draggedHandle) return;
+
+    const rect = canvas.getBoundingClientRect();
+    const inputX = x - rect.left - offsetX;
+    const inputY = y - rect.top - offsetY;
+    const graphRadius = getGraphRadius(canvas);
+
+    const angle = getCategoryAngle(draggedHandle.index, categoryCount);
+    
+    const lineStart = {
+        x: canvas.width / 2,
+        y: canvas.height / 2
+    }
+
+    const lineEnd = {
+        x: lineStart.x + graphRadius * Math.cos(angle),
+        y: lineStart.y + graphRadius * Math.sin(angle)
+    }
+
+    const APx = inputX - lineStart.x;
+    const APy = inputY - lineStart.y;
+    const ABx = lineEnd.x - lineStart.x;
+    const ABy = lineEnd.y - lineStart.y;
+    const ab2 = ABx * ABx + ABy * ABy;
+    let t = (APx * ABx + APy * ABy) / ab2;
+    t = Math.max(0, Math.min(1, t));
+
+    dataPoints[draggedHandle.index] = t;
+
+    draggedHandle.isValid = true;
+
+    draw();
+}
+
+function onInputUp() {
+    draggedHandle = null;
+}
+
+/**
 *   Redraw everything
 */
 function draw() {
